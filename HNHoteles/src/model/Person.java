0c9b02d12512cf84a5660d889fda06182103d288
Unknown <kmal.kenneth@live.com>
2017-12-01 12:47:47 -0600

    Add Proyect

---
+++ HNHoteles/src/model/Person.java
@@ -0,0 +1,55 @@
+package model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Person {
+
+    private IntegerProperty id;
+    private StringProperty name;
+    private StringProperty surname;
+
+    public Person(int id, String name, String surname) {
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.surname = new SimpleStringProperty(surname);
+    }
+
+    public int getId() {
+        return id.get();
+    }
+
+    public IntegerProperty idProperty() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id.set(id);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getSurname() {
+        return surname.get();
+    }
+
+    public StringProperty surnameProperty() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname.set(surname);
+    }
+}
