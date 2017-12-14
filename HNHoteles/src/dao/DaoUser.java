3913a2f1a2502012240d584fd409bcdd227015d0
Kenet Acuña <kmal.kenneth@live.com>
2017-12-13 23:35:16 -0600

    Dao user Login

--- HNHoteles/src/dao/DaoUser.java
+++ HNHoteles/src/dao/DaoUser.java
@@ -57,7 +57,53 @@ public class DaoUser implements DaoUserInterface {
 
     @Override
     public User getUser(int id_User) {
+
         return null;
+    }
+
+    @Override
+    public User getUserLogin(String email, String password) {
+
+        User user = null;
+
+        try {
+            ConnectionMariaDB connectionMariaDB = ConnectionMariaDB.getInstance();
+            connectionMariaDB.conect();
+
+            Connection connection = connectionMariaDB.getConnection();
+
+            StringBuilder query = new StringBuilder();
+            query.append("SELECT * FROM user WHERE e-mail = ");
+            query.append(email);
+            query.append(" AND pasword = ");
+            query.append(password);
+
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(query.toString());
+
+            while (rs.next()) {
+
+                user = new User(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("surname"),
+                        rs.getString("gender"),
+                        rs.getInt("phone"),
+                        rs.getString("e-mail"),
+                        rs.getString("password"),
+                        rs.getBoolean("admin")
+                );
+            }
+
+            st.close();
+            connectionMariaDB.disconect();
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+        return user;
     }
 
     @Override
--- HNHoteles/src/dao/interfaces/DaoUserInterface.java
+++ HNHoteles/src/dao/interfaces/DaoUserInterface.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 public interface DaoUserInterface {
     public ArrayList<User> getAllUser();
     public User getUser(int id_User);
+    public User getUserLogin(String email, String password);
     public void updateUser(User user);
     public void deleteUser(User user);
     public void addUser(User user);
