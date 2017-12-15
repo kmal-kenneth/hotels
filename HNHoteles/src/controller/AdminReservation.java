0a98f7d1038867f17cdc6b7adf057d4692145b6a
Diego A. Martínez Nvarro <navarrodiegomartinez@gmail.com>
2017-12-15 13:55:56 -0600

    Pushh

---
+++ HNHoteles/src/controller/AdminReservation.java
@@ -0,0 +1,42 @@
+package controller;
+
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.TableColumn;
+        import javafx.scene.control.TableView;
+
+public class AdminReservation {
+
+    @FXML
+    private TableView<?> tableReservation;
+
+    @FXML
+    private TableColumn<?, ?> userC;
+
+    @FXML
+    private TableColumn<?, ?> entryC;
+
+    @FXML
+    private TableColumn<?, ?> departureC;
+
+    @FXML
+    private TableColumn<?, ?> nightsC;
+
+    @FXML
+    private TableColumn<?, ?> roomC;
+
+    @FXML
+    void showHotels(ActionEvent event) {
+    }
+
+    @FXML
+    void showReservations(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showRooms(ActionEvent event) {
+
+    }
+
+}
