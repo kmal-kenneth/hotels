405405b12b1f2b0a9e3bde8240d9e84c7418e3b8
Kenet Acuña <kmal.kenneth@live.com>
2017-12-15 13:55:51 -0600

    up

---
+++ HNHoteles/src/controller/AdminHotelsController.java
@@ -0,0 +1,161 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class AdminHotelsController {
+
+    @FXML
+    private ToggleButton start1;
+
+    @FXML
+    private Button addHotels;
+
+    @FXML
+    private Button updateHotels;
+
+    @FXML
+    private ListView<?> attractiveListView;
+
+    @FXML
+    private TextField sicetxt;
+
+    @FXML
+    private TextField checkintxt;
+
+    @FXML
+    private ListView<?> lodgingListView;
+
+    @FXML
+    private TextField checkouttxt;
+
+    @FXML
+    private TextField addresstxt;
+
+    @FXML
+    private Button refreshHotels;
+
+    @FXML
+    private ToggleButton start3;
+
+    @FXML
+    private TextField coutrytxt;
+
+    @FXML
+    private ToggleButton start2;
+
+    @FXML
+    private ToggleButton start5;
+
+    @FXML
+    private ToggleButton start4;
+
+    @FXML
+    private TextArea requirementstxt;
+
+    @FXML
+    private TextField nametxt;
+
+    @FXML
+    private ToggleGroup start;
+
+    @FXML
+    private ListView<?> serviceListView;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private TextField servicetxt;
+
+    @FXML
+    private Button deleteHotels;
+
+    @FXML
+    private ListView<?> hotelsListView;
+
+    @FXML
+    private TextField phonetxt;
+
+    @FXML
+    private TextField attractivetxt;
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private TextField lodgingtxt;
+
+    @FXML
+    void addLodging(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updatelodging(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletelLdging(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addService(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateService(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteService(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addAttractive(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateAttractive(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteAttractive(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showHotels(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showRooms(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showResrvation(ActionEvent event) {
+
+    }
+
+}
